67d39d6d3729734013207a3963be471120b8def0
runaroundhome <16973228+runaroundhome@users.noreply.github.com>
2025-08-18 12:50:29 +1200

    Fix ~ not expanding

--- src/torrent-done.py
+++ src/torrent-done.py
@@ -2,19 +2,21 @@
 
 from sys import argv, exit
 import json
+import os
 
 
 def main():
     torrent_hash = argv[5].upper()
+    cache_file = os.path.expanduser("~/.orpheusmorebetter/cache-crawl")
 
     # find the hash and set done = true
-    with open("~/.orpheusmorebetter/cache-crawl", "r") as f:
+    with open(cache_file, "r") as f:
         cache = json.load(f)
 
     for torrent in cache:
         if torrent["hash"] == torrent_hash:
             torrent["done"] = True
-            with open("~/.orpheusmorebetter/cache-crawl", "w") as f:
+            with open(cache_file, "w") as f:
                 json.dump(cache, f)
             exit(0)
 
